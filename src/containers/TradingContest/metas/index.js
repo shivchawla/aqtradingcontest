39dfbbc6079b5117bcad9387e35ab8568eff5895
Shiv Chawla <shivchawla2001@gmail.com>
2018-11-20 19:53:14 +0530

    Update meta tags/title for contest

--- src/containers/TradingContest/metas/index.js
+++ src/containers/TradingContest/metas/index.js
@@ -5,9 +5,9 @@ export const DailyContestCreateMeta = props => {
     return (
         <Helmet>
             <meta charSet="utf-8" />
-            <title>AdviceQube: Daily Trading Contest</title>
-            <meta name="description" content="Daily Trading Contest for trading enthusiast"/>
-            <meta name="keywords" content="www.adviceqube.com, Investments, Equity, Stock market, Share Market, NSE stocks, returns, gain, India, Investment Advice"/>
+            <title>AdviceQube: Predict | Stock Prediction Contest</title>
+            <meta name="description" content="Make stock predictions and win prizes"/>
+            <meta name="keywords" content="www.adviceqube.com, Investments, Equity, Stock market, Share Market, NSE stocks, returns, gain, India, Prediction, trading, contest, predict"/>
             <meta name="robots" content="index, follow" />
         </Helmet>
     );
@@ -17,9 +17,9 @@ export const DailyContestLeaderboardMeta = props => {
     return (
         <Helmet>
             <meta charSet="utf-8" />
-            <title>AdviceQube: Daily Trading Contest</title>
-            <meta name="description" content="Daily Trading Contest for trading enthusiast"/>
-            <meta name="keywords" content="www.adviceqube.com, Investments, Equity, Stock market, Share Market, NSE stocks, returns, gain, India, Investment Advice"/>
+            <title>AdviceQube: Leaderboard | Stock Prediction Contest</title>
+            <meta name="description" content="Leaderboard of stock prediction contest"/>
+            <meta name="keywords" content="www.adviceqube.com, Investments, Equity, Stock market, Share Market, NSE stocks, returns, gain, India, Leaderboard, trading, contest, predictions"/>
             <meta name="robots" content="index, follow" />
         </Helmet>
     );
@@ -29,9 +29,9 @@ export const DailyContestTopPicksMeta = props => {
     return (
         <Helmet>
             <meta charSet="utf-8" />
-            <title>AdviceQube: Daily Trading Contest</title>
-            <meta name="description" content="Daily Trading Contest for trading enthusiast"/>
-            <meta name="keywords" content="www.adviceqube.com, Investments, Equity, Stock market, Share Market, NSE stocks, returns, gain, India, Investment Advice"/>
+            <title>AdviceQube: Top Picks | Stock Prediction Contest</title>
+            <meta name="description" content="Top picks of stock prediction contest"/>
+            <meta name="keywords" content="www.adviceqube.com, Equity, Stock market, Share Market, NSE stocks, returns, gain, India, toppicks, trading, contest, predict, predictions"/>
             <meta name="robots" content="index, follow" />
         </Helmet>
     );
@@ -41,9 +41,9 @@ export const DailyContestmyPicksMeta = props => {
     return (
         <Helmet>
             <meta charSet="utf-8" />
-            <title>AdviceQube: Daily Trading Contest</title>
-            <meta name="description" content="Daily Trading Contest for trading enthusiast"/>
-            <meta name="keywords" content="www.adviceqube.com, Investments, Equity, Stock market, Share Market, NSE stocks, returns, gain, India, Investment Advice"/>
+            <title>AdviceQube: My Picks | Stock Prediction Contest</title>
+            <meta name="description" content="Your predictions for stock prediction contest"/>
+            <meta name="keywords" content="www.adviceqube.com, Equity, Stock market, Share Market, NSE stocks, returns, gain, India, trading, contest, predict, predictions"/>
             <meta name="robots" content="index, follow" />
         </Helmet>
     );
@@ -53,9 +53,9 @@ export const DailyContestHomeMeta = props => {
     return (
         <Helmet>
             <meta charSet="utf-8" />
-            <title>AdviceQube: Daily Trading Contest | Home</title>
-            <meta name="description" content="Daily Trading Contest for trading enthusiast"/>
-            <meta name="keywords" content="www.adviceqube.com, Investments, Equity, Stock market, Share Market, NSE stocks, returns, gain, India, Investment Advice"/>
+            <title>AdviceQube: Stock Prediction Contest</title>
+            <meta name="description" content="Stock predictions contest for trading enthusiast"/>
+            <meta name="keywords" content="www.adviceqube.com, Equity, Stock market, Share Market, NSE stocks, returns, gain, India, trading, contest, predict, predictions"/>
             <meta name="robots" content="index, follow" />
         </Helmet>
     );
