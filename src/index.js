a6622caf005e3738112f0e2fef13d733649772ee
Saurav Biswas <saru.sreyo@gmail.com>
2019-01-02 13:21:05 +0530

    Updated service workers

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import {BrowserRouter as Router} from 'react-router-dom';
 import './index.css';
 import 'react-loading-bar/dist/index.css'
 import App from './App';
-import register, {unregister} from './registerServiceWorker';
+import {register, unregister} from './registerServiceWorker';
 
 global.Promise = require('bluebird');
 
