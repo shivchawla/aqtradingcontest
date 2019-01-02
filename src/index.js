1bffa4925b319bda83f0b860891a25471fc098a0
Saurav Biswas <saru.sreyo@gmail.com>
2019-01-02 13:48:54 +0530

    minor modifications of registerServiceWorkers

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import {BrowserRouter as Router} from 'react-router-dom';
 import './index.css';
 import 'react-loading-bar/dist/index.css'
 import App from './App';
-import {register, unregister} from './registerServiceWorker';
+import registerServiceWorker from './registerServiceWorker';
 
 global.Promise = require('bluebird');
 
 ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
 // unregister();
-register();
+registerServiceWorker();
