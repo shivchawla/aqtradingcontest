f9748c72f7203a810379e10126d62774daa50c44
Saurav Biswas <saru.sreyo@gmail.com>
2019-01-04 19:43:34 +0530

    SW mods

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -26,9 +26,7 @@ export default function register(event) {
     console.log('Window Path Name', window.location.pathname);
     console.log('Home Url', home);
     console.log('Equals', (window.location.pathname === home || window.location.pathname === '/_home'));
-    if (publicUrl.origin !== window.location.origin 
-      || (window.location.pathname === home || window.location.pathname === '/_home')
-    ) {
+    if (publicUrl.origin !== window.location.origin) {
       // Our service worker won't work if PUBLIC_URL is on a different origin
       // from what our page is served on. This might happen if a CDN is used to
       // serve assets; see https://github.com/facebookincubator/create-react-app/issues/2374
@@ -37,8 +35,8 @@ export default function register(event) {
     }
 
     window.addEventListener('fetch', function(event) {
-      if ( event.request.url.indexOf( '/blog/' ) !== -1 ) {
-        return false;
+      if (window.location.pathname === home || window.location.pathname === '/_home') {
+        console.log('Entered Home');
       }
     });
 
