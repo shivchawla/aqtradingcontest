625fdbf711abfba0c770230342c85717c3f1ceae
Saurav Biswas <saru.sreyo@gmail.com>
2019-01-04 19:27:34 +0530

    SW mods

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -25,9 +25,9 @@ export default function register(event) {
     const home = "/home";
     console.log('Window Path Name', window.location.pathname);
     console.log('Home Url', home);
-    console.log('Equals', (window.location.pathname === home || window.location.pathname !== '/_home'));
+    console.log('Equals', (window.location.pathname === home || window.location.pathname === '/_home'));
     if (publicUrl.origin !== window.location.origin 
-      && (window.location.pathname !== home || window.location.pathname !== '/_home')
+      && (window.location.pathname === home || window.location.pathname === '/_home')
     ) {
       // Our service worker won't work if PUBLIC_URL is on a different origin
       // from what our page is served on. This might happen if a CDN is used to
