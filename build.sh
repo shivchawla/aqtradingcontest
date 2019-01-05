387b23b36e17e9aaeca48073c3707b31f008e4be
Saurav Biswas <saru.sreyo@gmail.com>
2019-01-05 09:54:30 +0530

    Added custom build from remote Change home frame url to staging

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
+# export GENERATE_SOURCEMAP=false;
+# npm run build &> build.out
+# sed -i s:/static:/dailycontest/static:g build/index.html
+cp src/extraUtils/webpack.config.prod.js node_modules/react-scripts/config/webpack.config.prod.js
 export GENERATE_SOURCEMAP=false;
 npm run build &> build.out
-sed -i s:/static:/dailycontest/static:g build/index.html
