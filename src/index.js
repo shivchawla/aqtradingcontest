f8b0c84b93195eb467b512f54f0dee48512b3da6
Saurav Biswas <saru.sreyo@gmail.com>
2019-01-16 10:12:19 +0530

    Unregistered service worker

--- src/index.js
+++ src/index.js
@@ -5,12 +5,12 @@ import './index.css';
 import 'react-loading-bar/dist/index.css'
 import App from './App';
 import {Event} from './utils/events';
-import registerServiceWorker from './registerServiceWorker';
+import registerServiceWorker, {unregister} from './registerServiceWorker';
 
 global.Promise = require('bluebird');
 
 const event = new Event();
 
 ReactDOM.render(<Router><App event={event}/></Router>, document.getElementById('root'));
-// unregister();
-registerServiceWorker(event);
+unregister();
+// registerServiceWorker(event);
