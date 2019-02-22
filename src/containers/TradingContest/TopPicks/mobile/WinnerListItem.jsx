070570917f25bee6737f6720b64694c81b74c3e1
Saurav Biswas <saru.sreyo@gmail.com>
2019-02-22 12:52:09 +0530

    Using latestDetailRT to get change and changePct

--- src/containers/TradingContest/TopPicks/mobile/WinnerListItem.jsx
+++ src/containers/TradingContest/TopPicks/mobile/WinnerListItem.jsx
@@ -30,8 +30,8 @@ export default class WinnerListItem extends React.Component {
         const symbol = getStockTicker(security);
         const name = _.get(security, 'detail.Nse_Name', null) || '';
         const lastPrice = _.get(security, 'latestDetailRT.current', null) || _.get(security, 'latestDetail.Close', 0);
-        const change = Utils.formatMoneyValueMaxTwoDecimals(_.get(security, 'latestDetail.Change', 0));
-        const unformattedChangePct = _.get(security, 'latestDetail.ChangePct', 0.0);
+        const change = Utils.formatMoneyValueMaxTwoDecimals(_.get(security, 'latestDetailRT.change', null) || _.get(security, 'latestDetail.Change', 0));
+        const unformattedChangePct = _.get(security, 'latestDetailRT.changePct', null) ||  _.get(security, 'latestDetail.ChangePct', 0.0);
         const changePct = `(${(unformattedChangePct * 100).toFixed(2)}%)`;
         const changeColor = change > 0 ? metricColor.positive : change === 0 ? metricColor.neutral : metricColor.negative;
         const medal = getRankMedal(rank);
